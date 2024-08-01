c7f3e267ac169e544845a8ae3b235e05a6079e4a
ChetanPrince <saini.rajvir2@gmail.com>
2024-08-01 17:52:35 +0200

    button hitting search app

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 document.addEventListener("DOMContentLoaded",()=>{
 const input = document.querySelector("input[type='search']");
-
-
-
+const output = document.getElementById("output");
+const resultCounter = document.getElementById("resultCounter");
+const button = document.getElementById("button");
+button.addEventListener("click", (e)=>{
+e.preventDefault();
+const searchTerm = input.value;
+if(searchTerm){
+    searchWikipedia(searchTerm);
+}
+});
 
 });
